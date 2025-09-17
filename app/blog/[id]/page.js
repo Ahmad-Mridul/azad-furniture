222546f90674f1cd251119999c50fed8a4d32657
Ahmad-Mridul <mridulahmed262@gmail.com>
2025-09-17 16:02:43 +0600

    single blod detail page has been added

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,9 +1,138 @@
 import React from "react";
-
-const page = ({ params }) => {
+import "./blog-details.css";
+import blogs from "@/public/data/data.json";
+import Image from "next/image";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import Link from "next/link";
+const page = async ({ params }) => {
+    const { id } = await params;
+    const blog = blogs.find((blog) => blog.id.toString() === id);
+    console.log(blog);
     return (
-        <div>
-            <h1>Blog ID: {params.id}</h1>   
+        <div className="mt-8">
+            <div className="w-[75%] mx-auto">
+                <div className="space-y-5">
+                    <div className="flex items-center gap-5 px-25">
+                        <button className="px-4 py-1 rounded-xl bg-[#CFCFCF] border border-[#C77777]">
+                            Design
+                        </button>
+                        <p className="text-xl font-bold stroke-text">
+                            16 March 2025
+                        </p>
+                    </div>
+                    <h1 className="text-5xl font-bold px-25">{blog.title}</h1>
+                    <div className="relative w-full h-120">
+                        <Image
+                            src={blog.image}
+                            alt={blog.title}
+                            fill
+                            // style={{ objectFit: "cover" }}
+                            className="rounded-xl"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="w-[75%] mx-auto px-25 mt-16 font-thin">
+                <p>
+                    Google has been investing in AI for many years and bringing
+                    its benefits to individuals, businesses and communities.
+                    Whether it’s publishing state-of-the-art research, building
+                    helpful products or developing tools and resources that
+                    enable others, we’re committed to making AI accessible to
+                    everyone.
+                </p>
+                <br />
+                <p>
+                    We’re now at a pivotal moment in our AI journey.
+                    Breakthroughs in generative AI are fundamentally changing
+                    how people interact with technology — and at Google, we’ve
+                    been responsibly developing large language models so we can
+                    safely bring them to our products. Today, we’re excited to
+                    share our early progress. Developers and businesses can now
+                    try new APIs and products that make it easy, safe and
+                    scalable to start building with Google’s best AI models
+                    through Google Cloud and a new prototyping environment
+                    called MakerSuite. And in Google Workspace, we’re
+                    introducing new features that help people harness the power
+                    of generative AI to create, connect and collaborate.
+                </p>
+                <div className="max-w-xl mx-auto my-8 p-6 border-l-4 border-purple-600 bg-gray-50">
+                    <p className="text-gray-800 italic text-lg leading-relaxed">
+                        “People worry that computers will get too smart and take
+                        over the world, but the real problem is that they’re too
+                        stupid and they’ve already taken over the world.”
+                    </p>
+                    <p className="mt-4 text-gray-600 font-semibold">
+                        — Pedro Domingos
+                    </p>
+                </div>
+                <p>
+                    More than 3 billion people already benefit from AI-powered
+                    features in Google Workspace, whether it’s using Smart
+                    Compose in Gmail or auto-generated summaries in Google Docs.
+                    Now, we’re excited to take the next step and bring a limited
+                    set of trusted testers a new set of features that makes the
+                    process of writing even easier. In Gmail and Google Docs,
+                    you can simply type in a topic you’d like to write about,
+                    and a draft will be instantly generated for you. So if
+                    you’re a manager onboarding a new employee, Workspace saves
+                    you the time and effort involved in writing that first
+                    welcome email. From there, you can elaborate upon or
+                    abbreviate the message or adjust the tone to be more playful
+                    or professional — all in just a few clicks. We’ll be rolling
+                    out these new experiences to testers in the coming weeks.
+                </p>
+                <div className="relative w-[90%] my-5 mx-auto h-80">
+                    <Image
+                        src={blog.image}
+                        alt={blog.title}
+                        fill
+                        // style={{ objectFit: "cover" }}
+                        className="rounded-xl"
+                    />
+                </div>
+                <p>
+                    We’re so excited by the potential of generative AI, and the
+                    opportunities it will unlock — from helping people express
+                    themselves creatively, to helping developers build brand new
+                    types of applications, to transforming how businesses and
+                    governments engage their customers and constituents. Stay
+                    tuned for more to come in the weeks and months ahead.
+                </p>
+            </div>
+            <div className="w-[90%] mx-auto py-20">
+                <div className="flex items-center justify-between">
+                    <p className="text-[#F05B3A] text-5xl font-bold">
+                        Related blog
+                    </p>
+                    <Link href="/blog" className="btn bg-[#808080] rounded-xl text-white">
+                        View All <FaLongArrowAltRight />
+                    </Link>
+                </div>
+                <div className="mt-5 grid grid-cols-3 gap-9">
+                    {blogs.slice(0,3).map((blog, idx) => (
+                        <div
+                            className="p-3 bg-[#FFF9ED] space-y-2 rounded-2xl shadow"
+                            key={idx}
+                        >
+                            <Image
+                                className="rounded-2xl"
+                                src={blog.image}
+                                alt=""
+                                width={400}
+                                height={250}
+                            />
+                            <Link href={`/blog/${blog.id}`}>
+                                <h2 className="text-2xl font-medium">
+                                    {blog.title}
+                                </h2>
+                            </Link>
+
+                            <p>{blog.content}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,56 +1,13 @@
 import React from "react";
 import heroBg from "@/public/about-us/hero.png";
-import blog1 from "@/public/about-us/blog1.png";
-import blog2 from "@/public/about-us/blog2.png";
-import blog3 from "@/public/about-us/blog3.png";
+// import blog1 from "@/public/about-us/blog1.png";
+// import blog2 from "@/public/about-us/blog2.png";
+// import blog3 from "@/public/about-us/blog3.png";
 import Image from "next/image";
 import Link from "next/link";
 
 // blog data
-const blogs = [
-    {
-        id: 1,
-        image: blog1,
-        title: "House boating on Lake Shasta",
-        content:
-            "The best way to spend a long 4th of July weekend. Wake boarding, swimming, barbecues, and bonfires.",
-    },
-    {
-        id: 2,
-        image: blog2,
-        title: "Mountain Hiking Adventure",
-        content:
-            "Exploring nature’s beauty while hiking through mountain trails and enjoying breathtaking views.",
-    },
-    {
-        id: 3,
-        image: blog3,
-        title: "Beachside Camping Experience",
-        content:
-            "Camping on the beach with friends, enjoying sunsets, beach games, and campfire stories.",
-    },
-    {
-        id: 4,
-        image: blog1,
-        title: "Family Picnic in the Park",
-        content:
-            "Spending quality time outdoors with family, delicious food, and fun activities.",
-    },
-    {
-        id: 5,
-        image: blog2,
-        title: "Road Trip Across California",
-        content:
-            "Driving along scenic routes, visiting iconic landmarks, and making lifelong memories.",
-    },
-    {
-        id: 6,
-        image: blog3,
-        title: "Winter Skiing Holiday",
-        content:
-            "Hitting the slopes, enjoying the snow, and relaxing in cozy cabins during the winter holidays.",
-    },
-];
+import blogs from "@/public/data/data.json"
 
 const page = () => {
     return (
@@ -73,6 +30,8 @@ const page = () => {
                             className="rounded-2xl"
                             src={blog.image}
                             alt=""
+                            width={400}
+                            height={250}
                         />
                         <Link href={`/blog/${blog.id}`}>
                             <h2 className="text-2xl font-medium">
